c8a57ec5ad2345224d6f3be09ed4ecf5c8b2e084
zkzkp <zk.sec@outlook.com>
2020-12-21 21:17:42 +0800

    test: lock binary get fn

--- src/lib.rs
+++ src/lib.rs
@@ -6,40 +6,9 @@ extern crate alloc;
 #[cfg(any(feature = "lock_binary", feature = "test_tool"))]
 extern crate std;
 
+pub mod locks;
 #[cfg(feature = "test_tool")]
 pub mod test_tool;
-pub mod locks {
-    //! pub use const CODE_HASH_SECP256K1_KECCAK256_SIGHASH_ALL_DUAL: [u8; 32]
-    //! pub use const CODE_HASH_SECP256K1_DATA: [U8; 32]
-
-    include!(concat!(env!("OUT_DIR"), "/code_hashes.rs"));
-
-    #[cfg(feature = "lock_binary")]
-    pub mod binary {
-        //! pub use const BUNDLED_CELL: Files
-
-        use std::borrow::Cow;
-
-        include!(concat!(env!("OUT_DIR"), "/bundled.rs"));
-
-        const BINARIES: [&str; 2] = [
-            "specs/cells/secp256k1_data",
-            "specs/cells/secp256k1_keccak256_sighash_all_dual",
-        ];
-
-        #[repr(u8)]
-        pub enum Binary {
-            Secp256k1Data = 1,
-            Secp256k1Keccak256SighashDual,
-        }
-
-        pub fn get(binary: Binary) -> Cow<'static, [u8]> {
-            BUNDLED_CELL
-                .get(BINARIES[binary as usize - 1])
-                .expect("should be packaged")
-        }
-    }
-}
 
 use ckb_std::dynamic_loading::{CKBDLContext, Symbol};
 
---
+++ src/locks.rs
@@ -0,0 +1,43 @@
+//! pub use const CODE_HASH_SECP256K1_KECCAK256_SIGHASH_ALL_DUAL: [u8; 32]
+//! pub use const CODE_HASH_SECP256K1_DATA: [U8; 32]
+
+include!(concat!(env!("OUT_DIR"), "/code_hashes.rs"));
+
+#[cfg(feature = "lock_binary")]
+pub mod binary {
+    //! pub use const BUNDLED_CELL: Files
+
+    use std::borrow::Cow;
+
+    include!(concat!(env!("OUT_DIR"), "/bundled.rs"));
+
+    const BINARIES: [&str; 2] = [
+        "specs/cells/secp256k1_data",
+        "specs/cells/secp256k1_keccak256_sighash_all_dual",
+    ];
+
+    #[repr(u8)]
+    pub enum Binary {
+        Secp256k1Data = 1,
+        Secp256k1Keccak256SighashDual,
+    }
+
+    pub fn get(binary: Binary) -> Cow<'static, [u8]> {
+        inner_get(binary).expect("should be packaged")
+    }
+
+    fn inner_get(binary: Binary) -> std::io::Result<Cow<'static, [u8]>> {
+        BUNDLED_CELL.get(BINARIES[binary as usize - 1])
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::{inner_get, Binary};
+
+        #[test]
+        fn test_get() {
+            assert!(inner_get(Binary::Secp256k1Data).is_ok());
+            assert!(inner_get(Binary::Secp256k1Keccak256SighashDual).is_ok());
+        }
+    }
+}
