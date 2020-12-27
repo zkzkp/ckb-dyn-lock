2c13d5df2f7063452fa466b4c8d4ea119de96361
zkzkp <zk.sec@outlook.com>
2020-12-27 20:05:26 +0800

    change: move dynamic_loading into dynamic_loading module

--- src/lib.rs
+++ src/lib.rs
@@ -6,45 +6,11 @@ extern crate alloc;
 #[cfg(any(feature = "lock_binary", feature = "test_tool"))]
 extern crate std;
 
+#[cfg(feature = "dynamic_loading")]
+pub mod dynamic_loading;
 pub mod locks;
 #[cfg(feature = "test_tool")]
 pub mod test_tool;
 
-#[derive(Debug)]
-pub enum Error {
-    DynamicLoading(ckb_std::dynamic_loading::Error),
-    ValidationFunctionNotFound,
-    ValidateFailure(i32),
-}
-
-use ckb_std::dynamic_loading::{CKBDLContext, Symbol};
-
-type Validate = unsafe extern "C" fn(args: *const u8, len: u64) -> i32;
-const VALIDATE: &[u8; 8] = b"validate";
-
-pub struct DynLock {
-    validate: Symbol<Validate>,
-}
-
-impl DynLock {
-    pub fn load<T>(context: &mut CKBDLContext<T>, code_hash: &[u8]) -> Result<Self, Error> {
-        let lock = context.load(code_hash).map_err(Error::DynamicLoading)?;
-
-        let validate: Symbol<Validate> = unsafe {
-            lock.get(VALIDATE)
-                .ok_or_else(|| Error::ValidationFunctionNotFound)?
-        };
-
-        Ok(DynLock { validate })
-    }
-
-    pub fn validate(&self, args: &[u8], args_size: u64) -> Result<(), Error> {
-        let f = &self.validate;
-        let error_code = unsafe { f(args.as_ptr(), args_size) };
-
-        if error_code != 0 {
-            return Err(Error::ValidateFailure(error_code));
-        }
-        Ok(())
-    }
-}
+#[cfg(feature = "dynamic_loading")]
+pub use dynamic_loading::*;
